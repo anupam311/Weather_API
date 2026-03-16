94198129e4f21bae532296563a7ffb6f0da4b185
Anupam311 <chaudharyanupam746@gmail.com>
2026-03-16 09:48:43 +0530

    Correcting alignment in script.js.

--- script.js
+++ script.js
@@ -10,45 +10,38 @@ document
 
 function getWeather(){
 
-const city = document.getElementById("cityInput").value.trim();
+    const city = document.getElementById("cityInput").value.trim();
+    const url =
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-const url =
-`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
 
-fetch(url)
+        if(data.cod == "404"){
+            document.getElementById("weatherResult").innerHTML = "City not found";
+            return;
+        }
 
-.then(response => response.json())
+        const temp = data.main.temp;
+        const weather = data.weather[0].description;
+        const humidity = data.main.humidity;
 
-.then(data => {
+        let emoji="☀";
 
-if(data.cod == "404"){
-document.getElementById("weatherResult").innerHTML = "City not found";
-return;
-}
+        if(weather.includes("cloud")) emoji="☁";
+        if(weather.includes("rain")) emoji="🌧";
+        if(weather.includes("snow")) emoji="❄";
+        if(weather.includes("storm")) emoji="⛈";
 
-const temp = data.main.temp;
-const weather = data.weather[0].description;
-const humidity = data.main.humidity;
+        document.getElementById("weatherResult").innerHTML =
+        `<h2>${data.name}</h2>
+        <div class="temp">${emoji} ${temp}°C</div>
+        <p>${weather}</p>
+        <p>Humidity: ${humidity}%</p>`;
 
-let emoji="☀";
-
-if(weather.includes("cloud")) emoji="☁";
-if(weather.includes("rain")) emoji="🌧";
-if(weather.includes("snow")) emoji="❄";
-if(weather.includes("storm")) emoji="⛈";
-
-document.getElementById("weatherResult").innerHTML =
-
-`
-<h2>${data.name}</h2>
-<div class="temp">${emoji} ${temp}°C</div>
-<p>${weather}</p>
-<p>Humidity: ${humidity}%</p>
-`;
-
-})
-
-.catch(error=>{
-document.getElementById("weatherResult").innerHTML="Error fetching weather";
-});
+    })
+    .catch(error=>{
+        document.getElementById("weatherResult").innerHTML="Error fetching weather";
+    });
 }
